4fa65f9f53305f9eb9e8ea8b5554d4c7d5cbbc22
MD Abdullah AL Mamun <47145788+ronssrony@users.noreply.github.com>
2024-10-15 14:28:41 +0600

    Update Oppvideocall.jsx

--- src/component/Profile/Oppvideocall.jsx
+++ src/component/Profile/Oppvideocall.jsx
@@ -3,7 +3,7 @@ import ReactPlayer from "react-player"
 import { useEffect ,useState } from "react"
 import peer from "../../service/peer"
 import { io } from "socket.io-client"
-const socket = io('http://localhost:3000')
+const socket = io('https://imazineblue.onrender.com')
 function Videocall() {
 const [mystream , setMystream] = useState(null)
 const [remotestream , setRemotestream] = useState(null)
