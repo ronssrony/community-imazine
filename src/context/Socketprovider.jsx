f38d2cb059dc50548c896daf2c6046232ab101d5
MD Abdullah AL Mamun <47145788+ronssrony@users.noreply.github.com>
2024-10-15 14:51:40 +0600

    Update Socketprovider.jsx

--- src/context/Socketprovider.jsx
+++ src/context/Socketprovider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("http://localhost:3000"), []);
+  const socket = useMemo(() => io("https://imazine.netlify.app"), []);
 
   return (
     <Socketcontext.Provider value={{ socket }}>
