999e6024f553531646ad07f443cc456a93ce2cbc
MD Abdullah AL Mamun <47145788+ronssrony@users.noreply.github.com>
2024-10-15 14:29:13 +0600

    Update People.jsx

--- src/component/Profile/People.jsx
+++ src/component/Profile/People.jsx
@@ -6,13 +6,12 @@ import { useState } from 'react'
 import { RiCircleFill } from '@remixicon/react'
 import useFetch from '../utils/useFetch'
 
-
-const socket = io('http://localhost:3000')
+const socket = io('https://imazineblue.onrender.com')
 function People({data }) {
   const [receivemessage , setReceivemessage] = useState([])
   const [senderId , setSenderId] = useState([])
   const [active, setActive]  = useState()
-  const { data:activestatus , ispending , error}=useFetch(`http://localhost:3000/api/activestatus/${data._id}`) 
+  const { data:activestatus , ispending , error}=useFetch(`https://imazineblue.onrender.com/api/activestatus/${data._id}`) 
 
   useEffect(()=>{
     socket.on('active_userlist', (list)=>{
@@ -29,7 +28,7 @@ function People({data }) {
     })
     socket.on('popmessage',(data)=>{
         console.log(data)      
-        fetch('http://localhost:3000/api/fetchuserid',{
+        fetch('https://imazineblue.onrender.com/api/fetchuserid',{
           credentials:'include'
         }).then((res)=>{
           if(res.ok) return res.json()
@@ -49,7 +48,7 @@ function People({data }) {
     <div className=''>
     <div className='flex items-center gap-5'>
         <Link to={`/message/${data._id}`} className='flex items-center gap-2 max-sm:w-full  max-lg:w-1/2 w-1/6 truncate'> 
-            <img className="w-12 h-12 rounded-[50%] object-cover" src={`http://localhost:3000/images/uploads/${data.photo}`} alt="" />
+            <img className="w-12 h-12 rounded-[50%] object-cover" src={`https://imazineblue.onrender.com/images/uploads/${data.photo}`} alt="" />
         <div>
         {data.name && <h1  className=" text-lg  text-wrap ">{data.name}</h1>}
         {data._id===senderId ? <p1 className='absolute text-[14px] font-semibold'>{receivemessage} </p1> : <h1></h1> }
